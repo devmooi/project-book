7025d3f48d8b53e3c5d7cb8c40fa0dae832b5b1a
ednadev <ednadev@naver.com>
2020-09-03 10:36:06 +0900

    회원가입 단위테스트

---
+++ back-end/src/main/java/com/ednadev/book/domain/BookUser.java
@@ -0,0 +1,43 @@
+package com.ednadev.book.domain;
+
+public class BookUser {
+	private String userEmail, userPass, userImg, userName;
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getUserPass() {
+		return userPass;
+	}
+
+	public void setUserPass(String userPass) {
+		this.userPass = userPass;
+	}
+
+	public String getUserImg() {
+		return userImg;
+	}
+
+	public void setUserImg(String userImg) {
+		this.userImg = userImg;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	@Override
+	public String toString() {
+		return "BookUser [userEmail=" + userEmail + ", userPass=" + userPass + ", userImg=" + userImg + ", userName="
+				+ userName + "]";
+	}
+}
--- back-end/src/test/java/com/ednadev/book/BackEndApplicationTests.java
+++ back-end/src/test/java/com/ednadev/book/BackEndApplicationTests.java
@@ -1,13 +1,33 @@
 package com.ednadev.book;
 
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ednadev.book.domain.BookUser;
 
 @SpringBootTest
 class BackEndApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws IOException {
+		Reader r = Resources.getResourceAsReader("config/SqlMapConfig.xml");
+		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(r);
+		SqlSession session = factory.openSession();
+		
+		BookUser bookUser = new BookUser();
+		bookUser.setUserEmail("ednadev@naver.com");
+		bookUser.setUserName("김미경");
+		bookUser.setUserPass("1234");
+		session.insert("UserMapper.insertUser", bookUser);
+		session.commit();
+		System.out.println("회원가입");
 	}
 
 }
